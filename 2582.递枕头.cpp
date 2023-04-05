d0e57854882c6206b81775518060279d69f4543a
Kenny3Shen <asdjkl740886@outlook.com>
2023-04-05 19:24:43 +0800

    2023-04-05

--- 2582.递枕头.cpp
+++ 2582.递枕头.cpp
@@ -61,7 +61,8 @@ class Solution
 public:
     int passThePillow(int n, int time)
     {
-        
+        time %= (n - 1) * 2;
+        return time < n ? time + 1 : n - 1 - (time % n);
     }
 };
 // @lc code=end
