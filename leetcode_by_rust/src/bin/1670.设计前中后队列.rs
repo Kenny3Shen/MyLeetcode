f26685bdd6817ed2fc51ba2557f6e3ea064b41bf
Kenny3Shen <shen.ss@outlook.com>
2023-11-28 17:31:16 +0800

    leetcode [1670] Rust

---
+++ leetcode_by_rust/src/bin/1670.设计前中后队列.rs
@@ -0,0 +1,164 @@
+/*
+ * @lc app=leetcode.cn id=1670 lang=rust
+ *
+ * [1670] 设计前中后队列
+ *
+ * https://leetcode.cn/problems/design-front-middle-back-queue/description/
+ *
+ * algorithms
+ * Medium (52.05%)
+ * Likes:    61
+ * Dislikes: 0
+ * Total Accepted:    14.2K
+ * Total Submissions: 24.3K
+ * Testcase Example:  '["FrontMiddleBackQueue","pushFront","pushBack","pushMiddle","pushMiddle","popFront","popMiddle","popMiddle","popBack","popFront"]\n' +
+  '[[],[1],[2],[3],[4],[],[],[],[],[]]'
+ *
+ * 请你设计一个队列，支持在前，中，后三个位置的 push 和 pop 操作。
+ * 
+ * 请你完成 FrontMiddleBack 类：
+ * 
+ * 
+ * FrontMiddleBack () 初始化队列。
+ * void pushFront (int val) 将 val 添加到队列的 最前面 。
+ * void pushMiddle (int val) 将 val 添加到队列的 正中间 。
+ * void pushBack (int val) 将 val 添加到队里的 最后面 。
+ * int popFront () 将 最前面 的元素从队列中删除并返回值，如果删除之前队列为空，那么返回 -1 。
+ * int popMiddle () 将 正中间 的元素从队列中删除并返回值，如果删除之前队列为空，那么返回 -1 。
+ * int popBack () 将 最后面 的元素从队列中删除并返回值，如果删除之前队列为空，那么返回 -1 。
+ * 
+ * 
+ * 请注意当有 两个 中间位置的时候，选择靠前面的位置进行操作。比方说：
+ * 
+ * 
+ * 将 6 添加到 [1, 2, 3, 4, 5] 的中间位置，结果数组为 [1, 2, 6, 3, 4, 5] 。
+ * 从 [1, 2, 3, 4, 5, 6] 的中间位置弹出元素，返回 3 ，数组变为 [1, 2, 4, 5, 6] 。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：
+ * ["FrontMiddleBackQueue", "pushFront", "pushBack", "pushMiddle",
+ * "pushMiddle", "popFront", "popMiddle", "popMiddle", "popBack", "popFront"]
+ * [[], [1], [2], [3], [4], [], [], [], [], []]
+ * 输出：
+ * [null, null, null, null, null, 1, 3, 4, 2, -1]
+ * 
+ * 解释：
+ * FrontMiddleBackQueue q = new FrontMiddleBackQueue();
+ * q.pushFront(1);   // [1]
+ * q.pushBack(2);    // [1, 2]
+ * q.pushMiddle(3);  // [1, 3, 2]
+ * q.pushMiddle(4);  // [1, 4, 3, 2]
+ * q.popFront ();     // 返回 1 -> [4, 3, 2]
+ * q.popMiddle ();    // 返回 3 -> [4, 2]
+ * q.popMiddle ();    // 返回 4 -> [2]
+ * q.popBack ();      // 返回 2 -> []
+ * q.popFront ();     // 返回 -1 -> [] （队列为空）
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 最多调用 1000 次 pushFront， pushMiddle， pushBack， popFront， popMiddle 和 popBack
+ * 。
+ * 
+ * 
+ */
+
+// @lc code=start
+use std::collections::VecDeque;
+
+struct FrontMiddleBackQueue {
+    left: VecDeque<i32>,
+    right: VecDeque<i32>,
+}
+
+impl FrontMiddleBackQueue {
+    fn new() -> Self {
+        FrontMiddleBackQueue {
+            left: VecDeque::new(),
+            right: VecDeque::new(),
+        }
+    }
+
+    // 调整长度，保证 0 <= right.len () - left.len () <= 1
+    // 从而保证可以在正中间插入删除元素
+    fn balance(&mut self) {
+        if self.left.len() > self.right.len() {
+            self.right.push_front(self.left.pop_back().unwrap());
+        } else if self.right.len() > self.left.len() + 1 {
+            self.left.push_back(self.right.pop_front().unwrap());
+        }
+    }
+
+    fn push_front(&mut self, val: i32) {
+        self.left.push_front(val);
+        self.balance();
+    }
+
+    fn push_middle(&mut self, val: i32) {
+        if self.left.len() < self.right.len() {
+            self.left.push_back(val);
+        } else {
+            self.right.push_front(val);
+        }
+    }
+
+    fn push_back(&mut self, val: i32) {
+        self.right.push_back(val);
+        self.balance();
+    }
+
+    fn pop_front(&mut self) -> i32 {
+        if self.right.is_empty () { // 整个队列为空
+            return -1;
+        }
+        let val = if self.left.is_empty() {
+            self.right.pop_front().unwrap()
+        } else {
+            self.left.pop_front().unwrap()
+        };
+        self.balance();
+        val
+    }
+
+    fn pop_middle(&mut self) -> i32 {
+        if self.right.is_empty () { // 整个队列为空
+            return -1;
+        }
+        if self.left.len() == self.right.len() {
+            return self.left.pop_back().unwrap();
+        }
+        self.right.pop_front().unwrap()
+    }
+
+    fn pop_back(&mut self) -> i32 {
+        if self.right.is_empty () { // 整个队列为空
+            return -1;
+        }
+        let val = self.right.pop_back().unwrap();
+        self.balance();
+        val
+    }
+}
+
+
+/**
+ * Your FrontMiddleBackQueue object will be instantiated and called as such:
+ * let obj = FrontMiddleBackQueue::new();
+ * obj.push_front(val);
+ * obj.push_middle(val);
+ * obj.push_back(val);
+ * let ret_4: i32 = obj.pop_front();
+ * let ret_5: i32 = obj.pop_middle();
+ * let ret_6: i32 = obj.pop_back();
+ */
+// @lc code=end
+
