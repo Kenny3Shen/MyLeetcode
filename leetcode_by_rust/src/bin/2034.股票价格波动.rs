fd6111b5b0ba1a8fc146a8780b4d7d96f28cd129
Kenny3Shen <shen.ss@outlook.com>
2024-01-16 00:01:51 +0800

    update

--- leetcode_by_rust/src/bin/2034.股票价格波动.rs
+++ leetcode_by_rust/src/bin/2034.股票价格波动.rs
@@ -32,11 +32,11 @@
  * 请你实现 StockPrice 类：
  * 
  * 
- * StockPrice() 初始化对象，当前无股票价格记录。
- * void update(int timestamp, int price) 在时间点 timestamp 更新股票价格为 price 。
- * int current() 返回股票 最新价格 。
- * int maximum() 返回股票 最高价格 。
- * int minimum() 返回股票 最低价格 。
+ * StockPrice () 初始化对象，当前无股票价格记录。
+ * void update (int timestamp, int price) 在时间点 timestamp 更新股票价格为 price 。
+ * int current () 返回股票 最新价格 。
+ * int maximum () 返回股票 最高价格 。
+ * int minimum () 返回股票 最低价格 。
  * 
  * 
  * 
@@ -52,15 +52,15 @@
  * 
  * 解释：
  * StockPrice stockPrice = new StockPrice();
- * stockPrice.update(1, 10); // 时间戳为 [1] ，对应的股票价格为 [10] 。
- * stockPrice.update(2, 5);  // 时间戳为 [1,2] ，对应的股票价格为 [10,5] 。
- * stockPrice.current();     // 返回 5 ，最新时间戳为 2 ，对应价格为 5 。
- * stockPrice.maximum();     // 返回 10 ，最高价格的时间戳为 1 ，价格为 10 。
- * stockPrice.update(1, 3);  // 之前时间戳为 1 的价格错误，价格更新为 3 。
+ * stockPrice.update (1, 10); // 时间戳为 [1] ，对应的股票价格为 [10] 。
+ * stockPrice.update (2, 5);  // 时间戳为 [1,2] ，对应的股票价格为 [10,5] 。
+ * stockPrice.current ();     // 返回 5 ，最新时间戳为 2 ，对应价格为 5 。
+ * stockPrice.maximum ();     // 返回 10 ，最高价格的时间戳为 1 ，价格为 10 。
+ * stockPrice.update (1, 3);  // 之前时间戳为 1 的价格错误，价格更新为 3 。
  * ⁠                         // 时间戳为 [1,2] ，对应股票价格为 [3,5] 。
- * stockPrice.maximum();     // 返回 5 ，更正后最高价格为 5 。
- * stockPrice.update(4, 2);  // 时间戳为 [1,2,4] ，对应价格为 [3,5,2] 。
- * stockPrice.minimum();     // 返回 2 ，最低价格时间戳为 4 ，价格为 2 。
+ * stockPrice.maximum ();     // 返回 5 ，更正后最高价格为 5 。
+ * stockPrice.update (4, 2);  // 时间戳为 [1,2,4] ，对应价格为 [3,5,2] 。
+ * stockPrice.minimum ();     // 返回 2 ，最低价格时间戳为 4 ，价格为 2 。
  * 
  * 
  * 
@@ -95,9 +95,9 @@ impl StockPrice {
     
     fn update(&mut self, timestamp: i32, price: i32) {
         self.cur = self.cur.max(timestamp);
-        if let Some(old) = self.time_price.insert(timestamp, price) {  // 无法 insert 时返回 Some(v)，表示需要更新旧的 timestamp 对应的 price
-            *self.price_cnt.entry(old).or_insert(0) -= 1;   // 旧的 price 对应的 cnt 减一
-            if *self.price_cnt.get(&old).unwrap() <= 0 {    // 如果旧的 price 对应的 cnt 为 0，表示当前股票价格已经没有这个 price 了，需要删除
+        if let Some (old) = self.time_price.insert (timestamp, price) {  // 无法 insert 时返回 Some (v)，表示需要更新旧的 timestamp 对应的 price
+            *self.price_cnt.entry (old).or_insert (0) -= 1;   // 旧的 price 对应的 cnt 减一
+            if *self.price_cnt.get (&old).unwrap () <= 0 {    // 如果旧的 price 对应的 cnt 为 0，表示当前股票价格已经没有这个 price 了，需要删除
                 self.price_cnt.remove(&old);    
             }
         }
--- leetcode_by_rust/src/bin/2085.统计出现过一次的公共字符串.rs
+++ leetcode_by_rust/src/bin/2085.统计出现过一次的公共字符串.rs
@@ -14,12 +14,12 @@
  * Testcase Example:  '["leetcode","is","amazing","as","is"]\n["amazing","leetcode","is"]'
  *
  * 给你两个字符串数组 words1 和 words2 ，请你返回在两个字符串数组中 都恰好出现一次 的字符串的数目。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：words1 = ["leetcode","is","amazing","as","is"], words2 =
  * ["amazing","leetcode","is"]
  * 输出：2
@@ -29,34 +29,34 @@
  * - "is" 在两个数组中都出现过，但在 words1 中出现了 2 次，不计入答案。
  * - "as" 在 words1 中出现了一次，但是在 words2 中没有出现过，不计入答案。
  * 所以，有 2 个字符串在两个数组中都恰好出现了一次。
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：words1 = ["b","bb","bbb"], words2 = ["a","aa","aaa"]
  * 输出：0
  * 解释：没有字符串在两个数组中都恰好出现一次。
- * 
- * 
+ *
+ *
  * 示例 3：
- * 
- * 
+ *
+ *
  * 输入：words1 = ["a","ab"], words2 = ["a","a","a","ab"]
  * 输出：1
  * 解释：唯一在两个数组中都出现一次的字符串是 "ab" 。
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 1 <= words1.length, words2.length <= 1000
  * 1 <= words1[i].length, words2[j].length <= 30
  * words1 [i] 和 words2 [j] 都只包含小写英文字母。
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -69,15 +69,14 @@ impl Solution {
         }
         for word in words2 {
             *map2.entry(word).or_insert(0) += 1;
-        Leet
-        let mut res = 0;
-        for (word, cnt) in map1{
-            if cnt == 1 && map2.get(&word) == Some(&1) {
-                res += 1;
+            let mut res = 0;
+            for (word, cnt) in map1 {
+                if cnt == 1 && map2.get(&word) == Some(&1) {
+                    res += 1;
+                }
             }
+            return res;
         }
-        return res;
     }
 }
 // @lc code=end
-
