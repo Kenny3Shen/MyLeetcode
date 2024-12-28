960b2c502e5c3a5bb66ca783e70f97fdc6c59d8c
Kenny3Shen <asdjkl740886@outlook.com>
2024-12-28 23:56:31 +0800

    2024/12/28 [3046]

---
+++ 3046.split-the-array.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=3046 lang=python3
+# @lcpr version=30204
+#
+# [3046] 分割数组
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def isPossibleToSplit(self, nums: List[int]) -> bool:
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [1,1,2,2,3,4]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1,1,1,1]\n
+# @lcpr case=end
+
+#
+
