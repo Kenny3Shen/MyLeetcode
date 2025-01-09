e4df50fd7c80e4d1352d51b1e77f1bd6e2ff46c0
Kenny3Shen <asdjkl740886@outlook.com>
2025-01-10 00:53:24 +0800

    新增：实现二叉树展开为链表的解决方案

---
+++ leetcode_by_python/114.flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode.cn id=114 lang=python3
+# @lcpr version=30204
+#
+# [114] 二叉树展开为链表
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    head = None
+
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        if root is None:
+            return
+        self.flatten(root.right)
+        self.flatten(root.left)
+        root.left = None
+        root.right = self.head  # 头插法，相当于链表的 root.next = head
+        self.head = root  # 现在链表头节点是 root
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [1,2,5,3,4,null,6]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# []\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [0]\n
+# @lcpr case=end
+
+#
+
